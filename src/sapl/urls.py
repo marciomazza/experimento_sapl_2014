b3f22bbb92dd649f05f7dcc3a877df21c14af283
Marcio Mazza <mazza@interlegis.leg.br>
2014-06-07 17:48:50 -0300

    Adicionada tela de login basica

--- src/sapl/urls.py
+++ src/sapl/urls.py
@@ -6,4 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'home.views.index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
 )
