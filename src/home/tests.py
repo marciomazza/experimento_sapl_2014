5624f40a3a2b783e9553478354ddcd4cab72e676
Marcio Mazza <mazza@interlegis.leg.br>
2014-06-13 19:01:53 -0300

    Teste login e logout redirecionam para a home por padrao

--- src/home/tests.py
+++ src/home/tests.py
@@ -38,36 +38,44 @@ class LoginTestCase(WebTest):
         self.assertContains(response, 'Joao Firmino')
         self.assertContains(response, '<a href="/logout/?next=/">Sair</a>', html=True)
 
-    def do_login_form(self, next):
+    def do_login_form(self, uri):
         # estamos na página '/zzzz'
-        pagina_login = self.app.get('/login/?next=' + next)
+        pagina_login = self.app.get(uri)
         form = pagina_login.forms['login-form']
         form['username'], form['password'] = 'jfirmino', '123'
         return form.submit()
 
     def test_login_funciona(self):
-        self.do_login_form(next='/zzzz')
+        self.do_login_form('/login/?next=/zzzz')
         self.assertEquals(self.user.pk, self.app.session['_auth_user_id'])
 
     def test_login_redireciona_para_origem(self):
-        res = self.do_login_form(next='/zzzz')
+        res = self.do_login_form('/login/?next=/zzzz')
         self.assertEquals(res.url, 'http://testserver/zzzz')
 
     def test_login_da_propria_pagina_de_login_redireciona_para_home(self):
         # estamos na página '/login' e clicamos no link Login
-        res = self.do_login_form(next='/login/')
+        res = self.do_login_form('/login/?next=/login/')
         self.assertEquals(res.url, 'http://testserver/')
 
-    def do_logout(self, next):
+    def test_login_redireciona_para_home_por_padrao(self):
+        res = self.do_login_form('/login/')
+        self.assertEquals(res.url, 'http://testserver/')
+
+    def do_logout(self, uri):
         self.assertTrue(self.client.login(username='jfirmino', password='123'))
         # o usuário está logado
         self.assertEquals(self.user.pk, self.client.session['_auth_user_id'])
-        return self.client.get('/logout/?next=' + next)
+        return self.client.get(uri)
 
     def test_logout_funciona(self):
-        self.do_logout('/')
+        self.do_logout('/logout/?next=/')
         self.assertTrue('_auth_user_id' not in self.client.session)
 
     def test_logout_redireciona_para_origem(self):
-        res = self.do_logout('/zzzz')
+        res = self.do_logout('/logout/?next=/zzzz')
         self.assertEquals(res.url, 'http://testserver/zzzz')
+
+    def test_logout_redireciona_para_home_por_padrao(self):
+        res = self.do_logout('/logout/')
+        self.assertEquals(res.url, 'http://testserver/')
